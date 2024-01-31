324ace981816ac39dae65c593ad49f58e32ab102
Your Name <you@example.com>
2024-01-31 11:26:21 +0400

    Добавьте файлы проекта.

---
+++ LoupeTest1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System;
+using Gibraltar.Agent;
+using Loupe.Agent.AspNetCore;
+using Loupe.Agent.Core.Services;
+using Loupe.Agent.EntityFrameworkCore;
+using Loupe.Agent.PerformanceCounters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GibraltarTest;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        catch (Exception ex)
+        {
+            // Log the exception
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .AddLoupe(builder => builder.AddAspNetCoreDiagnostics()
+                .AddClientLogging()                     // The Loupe endpoint for client logging
+                .AddEntityFrameworkCoreDiagnostics()    // EF Core monitoring
+                .AddPerformanceCounters())              // Windows Perf Counter monitoring
+            .ConfigureServices((hostContext, services) =>
+            {
+                // Log an informational message
+                var logger = services.BuildServiceProvider().GetService<ILogger<Program>>();
+                logger.LogInformation("Application starting up...");
+            });
+}
+
---
+++ LoupeTest1/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using Gibraltar.Agent;
+using Loupe.Agent.Core.Services;
+using Loupe.Agent.EntityFrameworkCore;
+using Loupe.Agent.PerformanceCounters;
+using Loupe.Extensions.Logging;
+
+namespace GibraltarTest
+{
+    internal class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+
+
+            // Adding a simple logging statement
+            Console.WriteLine("Logging: ConfigureServices method is called.");
+        }
+    }
+}
